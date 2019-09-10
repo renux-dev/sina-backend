95066f88d66c13020f7d4d12cd4354b13490c93f
aliffrhn <alif.farhanw@gmail.com>
2019-09-10 15:21:23 +0700

    last commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 const app = express();
 
 // Route Handler
-var CertificateRouter = require('./routes/certificate');
+var SinaRouter = require('./routes/user');
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 // parse requests of content-type - application/json
@@ -15,21 +15,10 @@ app.use(bodyParser.json({limit: '50mb'}))
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 // Route Processing
-app.use('/v1/certificate', CertificateRouter);
+app.use('/v1/sina', SinaRouter);
 
 // listen for requests
 app.listen(port);
-
-sequelize
-.authenticate()
-.then(() => {
-    console.log('Connection has been established successfully.');
-})
-.catch(err => {
-    console.error('Unable to connect to the database:', err);
-    console.log(port)
-});
-
 
 module.exports = app
 
---
+++ routes/user.js
@@ -0,0 +1,20 @@
+var express   = require('express');
+var router    = express.Router();
+// var moment    = require('moment-timezone');
+
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host : '127.0.0.1',
+        user : 'root',
+        password : '',
+        database : 'sina'
+    }
+});
+
+router.get('/', (req,res) => {
+    console.log("ayam")
+})
+
+
+module.exports = router
